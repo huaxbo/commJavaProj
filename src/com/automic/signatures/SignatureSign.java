48ec953d32bc0867fab9f9d1d4b23a3c54530e54
huaxb <865507207@qq.com>
2016-11-24 17:03:55 +0800

    增加数字签名

--- src/com/automic/signatures/SignatureSign.java
+++ src/com/automic/signatures/SignatureSign.java
@@ -7,22 +7,22 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.interfaces.RSAPublicKey;
 
 import com.automic.util.CommonUtil;
 
-public class SignatureManager {
-
-	private final String password = "$%^&*(wert";
+public class SignatureSign {
+	
 	private KeyPair kp;
 	private Signature sg;
 	
 	/**
 	 * 
 	 */
-	public SignatureManager(){
+	public SignatureSign(){
 		try {
 			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-			kpg.initialize(1024,new SecureRandom(password.getBytes()));
+			kpg.initialize(1024,new SecureRandom());
 			kp = kpg.generateKeyPair();
 			sg = Signature.getInstance("SHA1WithRSA");
 			
@@ -57,43 +57,42 @@ public class SignatureManager {
 	}
 	
 	/**
-	 * 校验签名
-	 * @param mess
-	 * @param sign
+	 * 获取publicKey modulus
 	 * @return
 	 */
-	public boolean verifyMess(String mess,String sign){
-		if(mess == null || sign == null){
-			
-			return false;
-		}
-		try {
-			sg.initVerify(kp.getPublic());
-			sg.update(mess.getBytes());
-			
-			return sg.verify(CommonUtil.hexStr2byte(sign));
-			
-		} catch (InvalidKeyException | SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public String getPublicModulus(){
 		
+		RSAPublicKey pk = (RSAPublicKey)kp.getPublic();
 		
-		return false;
+		return pk.getModulus().toString(16);
 	}
+	
+	/**
+	 * 获取publicKey exponent
+	 * @return
+	 */
+	public String getPublicExponent(){
+		
+		RSAPublicKey pk = (RSAPublicKey)kp.getPublic();
+		
+		return pk.getPublicExponent().toString(16);
+	}	
 			
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SignatureManager smg = new SignatureManager();
+		SignatureSign smg = new SignatureSign();
 		String mess = "helloslllllefefe";
-		String sgm = smg.signatureMess(mess);
-		System.out.println("签名：" + sgm);
-		//sgm = "36b2be69bbea98c65b87b89117de98bb7a5c90095380688a3ac66a4ebc544360c99dd3c5f9fa4b1adba1c899b0af6c946900362aca1c814b384200a61045f5670b0a188e12218903900218cf1ffc11e20a4d626a7aac36b01d64d135c850fb4f19668264250834aa904810fe1b62b86052c0f356c8aa5b653b1f8486aa9bc962";
-//		mess = "helloslllllefef2";
-		System.out.println(smg.verifyMess(mess, sgm));
+		System.out.println("mess：" + mess);
+		String sign = smg.signatureMess(mess);
+		System.out.println("signature：" + sign);		
+		String modulus = smg.getPublicModulus();
+		String exponent = smg.getPublicExponent();			
+		
+		System.out.println("public modulus：" + modulus);
+		System.out.println("public exponent：" + exponent);
 	}
 
 }
